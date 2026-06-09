827481d709a0f15a2d0bf7adba8dac875856334a
Dmitry <ozerorakov@gmail.com>
2026-06-09 04:52:37 +0200

    fixed and isolated code for Windows, Linux still has no music

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,27 @@
+#ifdef _WIN32
 #include <windows.h>
 #include <mmsystem.h>
-
-#pragma comment(lib, "winmm.lib") 
+#pragma comment(lib, "winmm.lib")
+#endif
 
 #include "Game.h"
 
 int main()
 {
+    #ifdef _WIN32
+        // Enable ANSI escape codes in the Windows console
+        HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+        if (hOut != INVALID_HANDLE_VALUE) {
+            DWORD dwMode = 0;
+            if (GetConsoleMode(hOut, &dwMode)) {
+                dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+                SetConsoleMode(hOut, dwMode);
+            }
+        }
+
+        PlaySound(TEXT("assets/audio/DungeonRPG.wav"), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
+    #endif
+
     PlaySound(TEXT("assets/audio/DungeonRPG.wav"), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
 
     Game game;
